406333120bc2b07743bb968174b6402f670facbf
Chanda Hubbard <chanda.hubbard@gmail.com>
2020-07-02 15:09:27 -0700

    working on <BudgetTable/>

--- src/components/BudgetTable/BudgetTable.js
+++ src/components/BudgetTable/BudgetTable.js
@@ -1,30 +1,88 @@
 import React from "react";
 import "./budget-table.css";
-// import moment from "moment"
-import ExpenseContext from '../../contexts/ExpenseContext'
-
-
+import moment from "moment"
+import ExpenseContext from "../../contexts/ExpenseContext";
 
 export default class BudgetTable extends React.Component {
   static contextType = ExpenseContext;
-  render() {
-    const expenses = this.context
-    console.log(this.context, "budgettable")
-    console.log( "value")
+
   
+  // constructor(props) {
+  //   super(props);
+  //   console.log(this.context.expenses, "payload")
+  //   this.state = {
+  //     expenses: [],
+  //     handleDelete: () => {console.log('No delete')}
+  //   }
+  // }
+
+
+  // componentDidMount() {
+  //   if(this.context) {
+  //     let newExpenses = this.context.expenses;
+  //     let handleDelete = this.context.handleDeleteExpense;
+  //     this.setState({
+  //       expenses: newExpenses,
+  //       handleDelete
+  //     })
+  //   }
+
+
+  render() {
+    const expenses =  this.context;
+    console.log(this.context, "budgettable");
+    console.log("value");
+
     return (
       <ExpenseContext.Consumer>
-        
-      {(value) => (
-       
-  <div>budget table
-          {console.log(expenses, "insidetable")}
-        </div>
-      
-      )}
+        {(value) => (
+          <div>
+            budget table
+            {console.log(expenses, "insidetable")}
+            <table>
+  <thead>
+   <tr>
+     {/* Row 1 - Table Header*/}
+     <th scope="col">Date</th>
+     <th scope="col">Expense</th>
+     <th scope="col">Amount</th>
+     <th scope="col">Type</th>
+     <th scope="col">Category</th>
+     <th scope="col"> </th>
+     </tr>
+     </thead>
+     <tbody>
+       {/* Row 2 */}
+       {/* {expenses.map(expense => {
+      return (
+        <tr>
+          <td>{ moment(expense.date).format('MMMM D, YYYY') }</td>
+        <td>{expense.name}</td>
+        <td>{expense.amount}</td>
+        <td>{expense.type}</td>
+        <td>{expense.category}</td>
+        <td><button onClick={() => this.state.handleDelete(expense.id)} className="myButton">Delete</button></td>
+        </tr>
+      )
+      })
+  } */}
+            </tbody>
+          {/* Table footer */}
+          <tfoot>
+          <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          </tfoot>
+        </table>
+          </div>
+        )}
       </ExpenseContext.Consumer>
-      );
-    }
+    );
+  }
 
   // renderTable() {
   //   return (
@@ -41,9 +99,9 @@ export default class BudgetTable extends React.Component {
   //             <th scope="col"> </th>
   //           </tr>
   //         </thead>
-  //         <tbody> 
+  //         <tbody>
   //             {/* Row 2 */}
-              
+
   //             {
   //             this.state.
   //             expenses.map(expense => {
@@ -57,7 +115,7 @@ export default class BudgetTable extends React.Component {
   //       <td><button onClick={() => this.state.handleDelete(expense.id)} className="myButton">Delete</button></td>
   //       </tr>
   //     )
-  //     }) 
+  //     })
   // }
   //           </tbody>
   //         {/* Table footer */ }
@@ -74,30 +132,8 @@ export default class BudgetTable extends React.Component {
   //     </div>
   //   );
   // }
-  
-  //let expenses = await  props.expenses;
-  // constructor(props) {
-  //   super(props);
-  //   console.log(this.props.payload, "payload")
-  //   this.state = {
-  //     expenses: [],
-  //     handleDelete: () => {console.log('No delete')}
-  //   }
-  // }
 
-
-
-// componentDidMount() {
-//   if(this.props.payload) {
-//     let newExpenses = this.props.payload.expenses;
-//     let handleDelete = this.props.payload.handleDeleteExpense; 
-//     this.setState({
-//       expenses: newExpenses,
-//       handleDelete
-//     })
-//   } 
  
-// }
 
-
+  // }
 }
--- src/components/buttons/SignUpButton/SignupButton.js
+++ src/components/buttons/SignUpButton/SignupButton.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Swal from 'sweetalert2'
 
 export default class SignUpButton extends React.Component {
+  // const Swal = require('sweetalert2')
   signup() {
-    alert(
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
-    );
+    // alert(
+      // `Thank you for your interest.  Easy Budget is currently in the testing phase.  Sign up below and we will let you know once Easy Budget is available to the public`
+      Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+    // );
   }
   render() {
     return (
